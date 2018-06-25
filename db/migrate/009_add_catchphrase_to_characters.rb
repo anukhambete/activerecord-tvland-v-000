e0c73c8c300ac7281dd0c0350d471a1f867855d7
Anu Khambete <anukhambete@gmail.com>
2018-06-25 19:56:27 +0000

    automatically backed up by learn

--- db/migrate/009_add_catchphrase_to_characters.rb
+++ db/migrate/009_add_catchphrase_to_characters.rb
@@ -1,4 +1,4 @@
-class AddActorToCharacters < ActiveRecord::Migration[4.2]
+class AddCatchphraseToCharacters < ActiveRecord::Migration[4.2]
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
